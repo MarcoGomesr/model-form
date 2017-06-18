fecab1b196c138c0ac48479d824af3e980f67a7d
marcogomesr <marcogomesr@gmail.com>
2017-06-19 00:19:47 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+	form;
+
+	ngOnInit() {
+	  this.form = new FormGroup({
+			// firstName: new FormControl("", Validators.required),
+			firstName: new FormControl("", this.textValidation),
+			lastName: new FormControl("", Validators.compose([
+				Validators.required,
+				Validators.minLength(4),
+				Validators.pattern('[\\w\\-\\s\\/]+')
+			])),
+			languages: new FormControl("")
+		}) 
+  }
+
+  textValidation(control){
+  	if (control.value.length < 3) {
+  		return {'firstName': true}
+  	}
+  }
+  
+	
+  onSubmit(u){
+  	console.log(u)
+  }
 }
